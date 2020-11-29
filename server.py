626d46092ce8cc7ca190f19d38fc475324364117
amsuarez <amsuarez99@gmail.com>
2020-11-28 19:10:57 -0600

    Adds Entrance classes and sets up variables for mutual exclusion

--- server.py
+++ server.py
@@ -9,27 +9,71 @@
 import socket
 import sys
 import time
-
-# Definición de clases
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-""" Simula el manejo de préstamos bancarios. El gerente envía rápidamente una solicitud de préstamo ("aRequest") que lee de la
-	terminal, a uno de tres analistas según la cantidad solicitada. Utiliza Queues para amontonarles las solicitudes en orden a
-	cada uno de los analistas. Ellos corren en paralelo, cada uno en un thread
-"""
-
 import threading
 import time
 import random
 import queue
 
+#semaphore for changing free places variable
+sem = threading.Semaphore()
+lugares = 0
+globalTime = 0.0
 
 class Peticion():
     def __init__(self):
         self.timestamp = 0
-        self.ident = "Hola"
+        self.ident = "apertura"
+        self.numPuerta = -1
         self.detalle = []
+
+class PuertaEntrada():
+    def __init__(self):
+        self.requestQueue = queue.Queue(100)  # Entrance queue that handles requests
+
+    def activaPuerta(self):
+        while True:
+            # Waits for a request to arrive to the entrance
+            req = self.requestQueue.get()
+            if req is None:
+                break
+            # Attends oprimeBoton, recogeTarjeta, laserOffE, laserOnE
+            if req.ident == "oprimeBoton" or req.ident == "recogeTarjeta" or req.ident == "laserOnE" or req.ident == "laserOffE":
+                # critical section to modify available spaces and global time
+                sem.acquire()
+
+                print("in critical section")
+                time.sleep(1)
+                
+                sem.release()
+
+            # signals that entrance finishes processing request
+            self.requestQueue.task_done
+            time.sleep(1)
+
+
+class PuertaSalida():
+    def __init__(self):
+        self.requestQueue = queue.Queue(100)  # Queue of the entrance
+
+    def activaPuerta(self):
+        while True:
+            # Waits for a request to arrive to the entrance
+            req = self.requestQueue.get()
+            if req is None:
+                break
+            # Attends meteTarjeta, laserOffS, laserOnS
+            if req.ident == "meteTarjeta" or req.ident == "laserOnS" or req.ident == "laserOffS":
+                # critical section to modify available spaces and global time
+                sem.acquire()
+
+                print("In critical section")
+                time.sleep(1)
+
+                sem.release()
+
+            # signals that entrance finishes processing request
+            self.requestQueue.task_done
+            time.sleep(1)
 
 # Create a TCP/IP socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -55,21 +99,60 @@ connection, client_address = sock.accept()
 try:
     print('connection from', client_address)
 
-    # Receive the data
+    # First client message
+    data = connection.recv(256)
+    dataStr = data.decode('utf-8')
+    print('Servidor recibe "%s"' % dataStr)  # data bytes back to st
+
+    peticion = Peticion()
+    peticion.timestamp = float(dataStr.split()[0])
+    peticion.ident = dataStr.split()[1]
+    globalTime = peticion.timestamp
+    lugares = int(dataStr.split()[2])
+    numPuertasEntrada = int(dataStr.split()[3])
+    numPuertasSalida = int(dataStr.split()[4])
+
+    # Initializes proceses and they wait for response
+    puertasEntrada = []
+    puertasSalida = []
+    threads = []
+
+    for i in range(numPuertasEntrada):
+        puertasEntrada.append(PuertaEntrada())
+        t = threading.Thread(target=puertasEntrada[i].activaPuerta)
+        t.start()
+        threads.append(t)
+    for i in range(numPuertasSalida):
+        puertasSalida.append(PuertaSalida())
+        t = threading.Thread(target=puertasSalida[i].activaPuerta)
+        t.start()
+        threads.append(t)
+    print( "Servidor inicializa puertas ... " )
+    connection.sendall(bytes(peticion.ident, 'utf-8'))
     while True:
         data = connection.recv(256)
         dataStr = data.decode('utf-8')
-        print('server received "%s"' % dataStr)  # data bytes back to st
-
-        peticion = Peticion()
-        peticion.timestamp = float(dataStr.split()[0])
-        peticion.ident = dataStr.split()[1]
-        peticion.detalle = dataStr.split()[2:]
-
+        print('Servidor recibe "%s"' % dataStr)  # data bytes back to st
+    
         if data:
+            peticion = Peticion()
+            peticion.timestamp = float(dataStr.split()[0])
+            peticion.ident = dataStr.split()[1]
+            peticion.numPuerta = int(dataStr.split()[2])
+            peticion.detalle = dataStr.split()[3:]
+            
+            if peticion.ident == "meteTarjeta" or peticion.ident == "laserOffS" or peticion.ident == "laserOnS":
+                puertasSalida[peticion.numPuerta - 1].requestQueue.put(peticion)
+            elif peticion.ident == "cierre":
+                break
+            else:
+                puertasEntrada[peticion.numPuerta - 1].requestQueue.put(peticion)
+
+
             print('sending answer back to the client')
             # b converts str to bytes
-            connection.sendall(b'va de regreso...' + bytes(peticion.ident, 'utf-8'))
+            connection.sendall(b'va de regreso...' +
+                               bytes(peticion.ident, 'utf-8'))
         else:
             print('no data from', client_address)
             connection.close()
